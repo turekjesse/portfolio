afe3cb21521921e66f5fac965a57c82f234be310
turekjesse <turekjesse@gmail.com>
2021-07-13 14:02:21 -0400

    desktop side nav component complete

---
+++ portfolio-v2/src/components/Nav/Nav.js
@@ -0,0 +1,17 @@
+import {Link} from 'react-router-dom'
+
+const Nav = () => {
+
+    return (
+        <nav className="row-gap-10 flex-col border-r side-nav">
+            <Link to='/about'>About</Link>
+            <Link to='/projects'>Projects</Link>
+            <Link to='/experience'>Experience</Link>
+            <Link to='/hobbies'>Hobbies</Link>
+            <Link to='/contact'>Contact</Link>
+        </nav>
+    )
+
+}
+
+export default Nav
