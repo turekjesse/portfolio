63e33063002c1c729f85d468ed1d6ec61f35671a
turekjesse <turekjesse@gmail.com>
2021-07-23 23:18:16 -0400

    about me text added

--- portfolio-v2/src/components/About/About.js
+++ portfolio-v2/src/components/About/About.js
@@ -1,6 +1,6 @@
 const About = () => {
     
-    const text = '{ Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. }'
+    const text = 'I am a Software Engineer based in Brooklyn, NY with 5 years of prior experience in eCommerce and a life-long passion for building, fixing, and tinkering with things. My knack for mechanics paired with my meticulous attention to detail and creative problem solving skills were the driving forces that lead me to learn to code.'
 
     return (
         <main className='flex-row col-gap-20 about-container'>            
@@ -8,7 +8,6 @@ const About = () => {
                     <h1>Hey, I'm Jesse</h1>
                     <p className="indent">{text}</p>
                 </div>
-                {/* <div className="img-container"></div> */}
                 <img src='https://i.imgur.com/9l1PKOx.jpg' alt="" />
         </main>
     )
