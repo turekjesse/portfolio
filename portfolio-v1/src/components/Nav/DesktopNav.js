7297ba7ce0396c667a5ca2498d7409f9c60f9528
turekjesse <turekjesse@gmail.com>
2021-07-24 00:19:45 -0400

    code formatted

--- portfolio-v1/src/components/Nav/DesktopNav.js
+++ portfolio-v1/src/components/Nav/DesktopNav.js
@@ -1,36 +1,16 @@
-import { Menu } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const DesktopNav = () => {
-    return (
-        <Menu fluid widths={10} inverted>
-          <Menu.Item
-            name='home'
-            as={Link}
-            to="/"
-          />
-          <Menu.Item
-          as={Link}
-          to="/about"
-            name='about'
-          />
-          <Menu.Item
-          as={Link}
-          to="/projects"
-            name='projects'
-          />
-          <Menu.Item
-          as={Link}
-          to="/experience"
-            name='experience'
-          />
-          <Menu.Item
-          as={Link}
-          to="/hobbies"
-            name='hobbies'
-          />
-        </Menu>
-    )
-}
+  return (
+    <Menu fluid widths={10} inverted>
+      <Menu.Item as={Link} to="/" name="home" />
+      <Menu.Item as={Link} to="/about" name="about" />
+      <Menu.Item as={Link} to="/projects" name="projects" />
+      <Menu.Item as={Link} to="/experience" name="experience" />
+      <Menu.Item as={Link} to="/hobbies" name="hobbies" />
+    </Menu>
+  );
+};
 
-export default DesktopNav
+export default DesktopNav;
--- portfolio-v1/src/components/Nav/MobileNav.js
+++ portfolio-v1/src/components/Nav/MobileNav.js
@@ -1,55 +1,21 @@
-import { Dropdown, Icon, Menu } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import { Dropdown, Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 const MobileNav = () => {
-    return (
-            <Menu inverted>
-                <Dropdown item icon='bars' simple>
-                <Dropdown.Menu inverted>
-                    <Menu.Item
-                        name='home'
-                        as={Link}
-                        to="/"
-                    />
-                    <Menu.Item
-                        as={Link}
-                        to="/about"
-                        name='about'
-                    />
-                    <Menu.Item
-                        as={Link}
-                        to="/projects"
-                        name='projects'
-                    />
-                    <Menu.Item
-                        as={Link}
-                        to="/experience"
-                        name='experience'
-                    />
-                    <Menu.Item
-                        as={Link}
-                        to="/hobbies"
-                        name='hobbies'
-                    />
-                    {/* <Dropdown.Item inverted>
-                    <Icon name='dropdown' />
-                    <span className='text'>New</span>
-                    <Dropdown.Menu>
-                        <Dropdown.Item>Document</Dropdown.Item>
-                        <Dropdown.Item>Image</Dropdown.Item>
-                    </Dropdown.Menu>
-                    </Dropdown.Item>
-                    <Dropdown.Item>Open</Dropdown.Item>
-                    <Dropdown.Item>Save...</Dropdown.Item>
-                    <Dropdown.Item>Edit Permissions</Dropdown.Item>
-                    <Dropdown.Divider />
-                    <Dropdown.Header>Export</Dropdown.Header>
-                    <Dropdown.Item>Share</Dropdown.Item> */}
-                </Dropdown.Menu>
-                </Dropdown>
-            </Menu>
-        )
+  return (
+    <Menu inverted>
+      <Dropdown item icon="bars" simple>
+        <Dropdown.Menu inverted>
+          <Menu.Item name="home" as={Link} to="/" />
+          <Menu.Item as={Link} to="/about" name="about" />
+          <Menu.Item as={Link} to="/projects" name="projects" />
+          <Menu.Item as={Link} to="/experience" name="experience" />
+          <Menu.Item as={Link} to="/hobbies" name="hobbies" />
 
-}
+        </Dropdown.Menu>
+      </Dropdown>
+    </Menu>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
