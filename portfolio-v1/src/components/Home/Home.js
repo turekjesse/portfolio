22882beacadbe1084f3e31b6d46526166f32d2e4
turekjesse <turekjesse@gmail.com>
2021-07-24 14:02:43 -0400

    home layout initialized

--- portfolio-v1/src/components/Home/Home.js
+++ portfolio-v1/src/components/Home/Home.js
@@ -1,9 +1,59 @@
-
+import { Segment,Image, Header, Reveal, Card, Grid, Container } from "semantic-ui-react";
 
 const Home = () => {
-    return (
-        <p>this is the home component</p>
-    )
-}
+  return (
+    <>
+      <Grid centered inverted columns='equal'>
 
-export default Home
+
+        <Grid.Row padded>
+          <Grid.Column>
+            <Container className="grid-container about" fluid>
+            </Container>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row columns="equal">
+          <Grid.Column  mobile={16} computer={8}>
+          <Container className="grid-container projects" fluid>
+            </Container>
+          </Grid.Column>
+          <Grid.Column mobile={16} computer={8}>
+          <Container className="grid-container experience" fluid>
+            </Container>
+          </Grid.Column>
+
+        </Grid.Row>
+        <Grid.Row columns="equal">
+          <Grid.Column>
+            <Segment padded inverted textAlign="center">
+              <Header>Hobbies</Header>
+            </Segment>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row columns="equal">
+        <Grid.Column mobile={8} computer={4}>
+          <Container className="grid-container woodworking" fluid>
+            </Container>
+          </Grid.Column>
+          <Grid.Column mobile={8} computer={4}>
+          <Container className="grid-container cycling" fluid>
+            </Container>
+          </Grid.Column>
+          <Grid.Column mobile={8} computer={4}>
+          <Container className="grid-container hiking" fluid>
+            </Container>
+          </Grid.Column>
+          <Grid.Column mobile={8} computer={4}>
+          <Container className="grid-container surfing" fluid>
+            </Container>
+          </Grid.Column>
+        </Grid.Row>
+
+      </Grid>
+    </>
+  );
+};
+
+export default Home;
