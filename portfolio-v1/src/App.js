859e73baf839d3c69832209c45546af04762b7fc
turekjesse <turekjesse@gmail.com>
2021-07-24 00:10:45 -0400

    Brkpoint state added, main comps imported

--- portfolio-v1/src/App.js
+++ portfolio-v1/src/App.js
@@ -1,17 +1,40 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Route } from 'react-router-dom'
 import './App.css';
 
+import About from './components/About/About';
 import DesktopNav from './components/Nav/DesktopNav';
-import MobileNav from './components/Nav/MobileNav';
+import Experience from './components/Experience/Experience';
+import Hobbies from './components/Hobbies/Hobbies';
 import Home from './components/Home/Home';
+import MobileNav from './components/Nav/MobileNav';
+import Projects from './components/Projects/Projects';
 
 function App() {
 
+  const [width, setWidth] = useState(window.innerWidth)
+
+  const breakpoint = 720
+
+  const handleWindowResize = () => {
+    setWidth(window.innerWidth)
+    window.addEventListener('resize', handleWindowResize)
+  }
+
+  const killWindowResize = () => {
+    window.removeEventListener('resize', handleWindowResize)
+  } 
+
+  useEffect(() => {
+    handleWindowResize()
+    killWindowResize()
+  })
+
   return (
     <>
-    <DesktopNav/>
-    <MobileNav/>
+    {
+       width < breakpoint ? <MobileNav /> : <DesktopNav />
+    }
     <Home/>
     </>
   );
