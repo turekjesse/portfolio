335f482aaa6f2e25bf4e08e94f95e64a121ddd93
turekjesse <turekjesse@gmail.com>
2021-07-25 09:04:27 -0400

    uneeded components removed. Routes finalized

--- portfolio-v1/src/App.js
+++ portfolio-v1/src/App.js
@@ -3,43 +3,57 @@ import { Route } from 'react-router-dom'
 import './App.css';
 
 import About from './components/About/About';
-import DesktopNav from './components/Nav/DesktopNav';
 import Experience from './components/Experience/Experience';
 import Hobbies from './components/Hobbies/Hobbies';
 import Home from './components/Home/Home';
-import MobileNav from './components/Nav/MobileNav';
 import Projects from './components/Projects/Projects';
 
 function App() {
-
-  const [width, setWidth] = useState(window.innerWidth)
-
   const breakpoint = 720
 
-  const handleWindowResize = () => {
-    setWidth(window.innerWidth)
-    window.addEventListener('resize', handleWindowResize)
+  const useViewPortWidth = () => {
+    const [width, setWidth] = useState(window.innerWidth)
+  
+    const handleWindowResize = () => {
+      setWidth(window.innerWidth)
+      window.addEventListener('resize', handleWindowResize)
+    }
+  
+    const killWindowResize = () => {
+      window.removeEventListener('resize', handleWindowResize)
+    } 
+  
+    useEffect(() => {
+      handleWindowResize()
+      killWindowResize()
+    })
+
+    return { width };
   }
-
-  const killWindowResize = () => {
-    window.removeEventListener('resize', handleWindowResize)
-  } 
-
-  useEffect(() => {
-    handleWindowResize()
-    killWindowResize()
-  })
 
   return (
     <>
-    {
-       width < breakpoint ? <MobileNav /> : <DesktopNav />
-    }
     <Route path="/" exact component={Home}/>
-    <Route path="/about" exact component={About}/>
-    <Route path="/projects" exact component={Projects}/>
-    <Route path="/experience" exact component={Experience}/>
-    <Route path="/hobbies" exact component={Hobbies}/>
+    <Route
+      path="/about"
+      render={ () => <About breakpoint={breakpoint}
+      useViewPortWidth={useViewPortWidth} />}
+    />
+    <Route
+      path="/projects"
+      render={ () => <Projects breakpoint={breakpoint}
+      useViewPortWidth={useViewPortWidth} />}
+    />
+    <Route
+      path="/experience"
+      render={ () => <Experience breakpoint={breakpoint}
+      useViewPortWidth={useViewPortWidth} />}
+    />
+    <Route
+      path="/hobbies"
+      render={ () => <Hobbies breakpoint={breakpoint}
+      useViewPortWidth={useViewPortWidth} />}
+    />
     </>
   );
 }
