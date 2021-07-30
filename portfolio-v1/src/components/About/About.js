5f7ee031ee1095c86fe4993a9040ab7ec0a60f0b
turekjesse <turekjesse@gmail.com>
2021-07-29 21:55:41 -0400

    nav removed, about header + container updated

--- portfolio-v1/src/components/About/About.js
+++ portfolio-v1/src/components/About/About.js
@@ -1,5 +1,3 @@
-import MobileNav from "../Nav/MobileNav";
-import DesktopNav from "../Nav/DesktopNav";
 import { Grid, Image, Segment } from "semantic-ui-react";
 
 const About = ({ useViewPortWidth, breakpoint }) => {
@@ -15,9 +13,7 @@ const About = ({ useViewPortWidth, breakpoint }) => {
 
   return (
     <>
-      {width < breakpoint ? <MobileNav /> : <DesktopNav />}
       <Segment className="no-margin" inverted>
-        
         <Grid centered columns="equal">
           <Grid.Column
             style={width < breakpoint ? mobileBorder : desktopBorder}
@@ -26,20 +22,42 @@ const About = ({ useViewPortWidth, breakpoint }) => {
             computer={8}
           >
             <Segment textAlign="center" className="no-margin" inverted>
-              <Image size="large" centered fluid src="https://i.imgur.com/9l1PKOx.jpg" />
+              <Image
+                size="large"
+                centered
+                fluid
+                src="https://i.imgur.com/9l1PKOx.jpg"
+              />
             </Segment>
           </Grid.Column>
           <Grid.Column>
-            <Segment className="no-margin" inverted size="big">
+            <div className="about-container">
+              <Segment className="no-margin  about-header" inverted>
                 <h1>Hey, I'm Jesse</h1>
-            </Segment>
-            <Segment textAlign="center" size="large" inverted className="no-margin">
-                <p>I am a Brooklyn-Based Software Engineer with 5 years of prior experience in eCommerce and a life-long passion for building, fixing, and tinkering with things. My knack for mechanics paired with my meticulous attention to detail and creative problem solving skills were the driving forces that lead me to learn to code.</p>
-                <p>Check out the hobbies page for a glimpse into what I'm up to when I'm not building websites and apps.</p>
-            </Segment>
+              </Segment>
+              <Segment size="large" inverted className="no-margin">
+                <p>
+                  I am a Brooklyn-Based Software Engineer with 5 years of prior
+                  experience in eCommerce and a life-long passion for building,
+                  fixing, and tinkering with things. My knack for mechanics
+                  paired with my meticulous attention to detail and creative
+                  problem solving skills were the driving forces that lead me to
+                  learn to code.
+                </p>
+                <p>
+                  Check out the hobbies page for a glimpse into what I'm up to
+                  when I'm not building websites and apps.
+                </p>
+              </Segment>
             <Segment textAlign="center" className="no-margin" inverted>
-              <Image size="large" centered fluid src="https://i.imgur.com/gSnQ9Jo.jpg" />
+              <Image
+                size="big"
+                centered
+                fluid
+                src="https://i.imgur.com/gSnQ9Jo.jpg"
+              />
             </Segment>
+            </div>
           </Grid.Column>
         </Grid>
       </Segment>
