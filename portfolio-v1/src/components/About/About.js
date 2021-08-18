d990f62ea26cbae210444aa78d8fb60215fb4e75
turekjesse <turekjesse@gmail.com>
2021-08-18 11:24:51 -0400

    borders removed, hobbies added

--- portfolio-v1/src/components/About/About.js
+++ portfolio-v1/src/components/About/About.js
@@ -1,29 +1,31 @@
-import { Grid, Image, Segment } from "semantic-ui-react";
+import { Grid, Segment, Image } from "semantic-ui-react";
+import './About.css'
+import Surf from '../../assets/surf.mp4'
 
 const About = ({ useViewPortWidth, breakpoint }) => {
   const { width } = useViewPortWidth();
 
-  const mobileBorder = {
-    borderBottom: "1px solid rgba(255,255,255,.2)",
-  };
+  // const mobileBorder = {
+  //   borderBottom: "1px solid rgba(255,255,255,.2)",
+  // };
 
-  const desktopBorder = {
-    borderRight: "1px solid rgba(255,255,255,.2)",
-  };
+  // const desktopBorder = {
+  //   borderRight: "1px solid rgba(255,255,255,.2)",
+  // };
 
   return (
     <>
       <Segment className="no-margin" inverted>
         <Grid centered columns="equal">
           <Grid.Column
-            style={width < breakpoint ? mobileBorder : desktopBorder}
-            className="job-segment"
+            // style={width < breakpoint ? mobileBorder : desktopBorder}
+            className="about-segment"
             mobile={16}
             computer={8}
           >
             <Segment textAlign="center" className="no-margin" inverted>
               <Image
-                size="large"
+                size="big"
                 centered
                 fluid
                 src="https://i.imgur.com/9l1PKOx.jpg"
@@ -44,10 +46,6 @@ const About = ({ useViewPortWidth, breakpoint }) => {
                   problem solving skills were the driving forces that lead me to
                   learn to code.
                 </p>
-                <p>
-                  Check out the hobbies page for a glimpse into what I'm up to
-                  when I'm not building websites and apps.
-                </p>
               </Segment>
             <Segment textAlign="center" className="no-margin" inverted>
               <Image
@@ -58,7 +56,87 @@ const About = ({ useViewPortWidth, breakpoint }) => {
               />
             </Segment>
             </div>
+          </Grid.Column>  
+          <Grid.Column
+          // style={width < breakpoint ? mobileBorder : desktopBorder}
+            className="about-segment"
+            mobile={16}
+            computer={8}>
+            <div className="about-container">
+              <Segment size="large" inverted className="no-margin">
+                <p>
+                  When I'm not cracking coding, you can find me outdoors surfing, bikepacking, hiking, or skateboarding or in a workshop tuning up bikes, turning wooden bowls, or making custom furniture.
+                </p>
+                <p>
+                  It's rare that I'm ever inactive, but when I am, I'm relaxing on the couch or in the park with my dog, Asha.
+                </p>
+                <p>
+                  Check out the photos below for a glimpse into some of my adventures and creations.
+                </p>
+              </Segment>
+            <Segment textAlign="center" className="no-margin" inverted>
+            <Image
+                size="big"
+                centered
+                fluid
+                src="https://i.imgur.com/ueQJKbe.jpg"
+              />
+            </Segment>
+            </div>
           </Grid.Column>
+          <Grid.Column
+          >
+            <Segment textAlign="center" className="no-margin" inverted>
+              <Image
+                size="large"
+                centered
+                fluid
+                src="https://i.imgur.com/NgQQ7dM.jpg"
+              />
+            </Segment>
+          </Grid.Column> 
+          <Grid.Column
+            // style={width < breakpoint ? mobileBorder : desktopBorder}
+            className="about-segment"
+            mobile={16}
+            computer={8}
+          >
+            <Segment textAlign="center" className="no-margin" inverted>
+              <Image
+                size="large"
+                centered
+                fluid
+                src="https://i.imgur.com/E8Lp72A.jpg"
+              />
+            </Segment>
+          </Grid.Column>
+          <Grid.Column>
+            <div style={width > breakpoint ? {marginTop: '3rem'} : null} className="about-container">
+            <Segment textAlign="center" className="no-margin" inverted>
+              <Image
+                size="large"
+                centered
+                fluid
+                src="https://i.imgur.com/UbfkMpE.jpg"
+              />
+            </Segment>
+            <Segment textAlign="center" className="no-margin" inverted>
+              <Image
+                size="large"
+                centered
+                fluid
+                src="https://i.imgur.com/9CRF8ns.jpg"
+              />
+            </Segment>
+            </div>
+          </Grid.Column>
+          <Grid.Row>
+            <Segment inverted>
+              <video width='100%' src={Surf} controls loop="true">
+                Sorry, your browser doesn't support video.
+              </video>
+            </Segment>
+          </Grid.Row>
         </Grid>
       </Segment>
     </>
