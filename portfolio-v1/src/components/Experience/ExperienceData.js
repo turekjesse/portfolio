a7328d469a1afde1f1cb723d67b48e24292de221
turekjesse <turekjesse@gmail.com>
2021-08-18 18:09:43 -0400

    experience data isolated

---
+++ portfolio-v1/src/components/Experience/ExperienceData.js
@@ -0,0 +1,33 @@
+import GA_Logo2 from "../../assets/GA_Logo2.png";
+import carhartt_wip_text_logo from "../../assets/carhartt_wip_text_logo-white.png";
+import Unis from "../../assets/Unis.png";
+
+export const ExperienceData = [
+    {
+      company: "General Assembly",
+      title: "Software Engineering Immersive",
+      timeline: "5 / 2021 - 8 / 2021",
+      image: `${GA_Logo2}`,
+      description:
+        "Engaged in a 12 week, 500+ hour, remote software engineering immersive course while learning programming and computer science fundamentals in regards to full-stack development. Worked collaboratively in an agile development workflow using industry-standard version control tools. ",
+      skills: "HTML, CSS, JavaSript, MERN",
+    },
+    {
+      company: "Carhartt WIP",
+      title: "Content Manager",
+      timeline: "3 / 2018 - 5 / 2021",
+      image: `${carhartt_wip_text_logo}`,
+      description:
+        "Managed product data for 1000+ SKUs per season. Executed weekly site-wide content updates to align with sales-performance and marketing initiatives. Arranged and built biweekly newsletter campaigns utilizing Sketch and Mailchimp ESP. Coordinated feature deployments with external dev team.",
+      skills: "Shopify, Mailchimp, Excel, Sketch, Photoshop, Amplience CMS",
+    },
+    {
+      company: "Unis New York",
+      title: "Ecommerce Manager",
+      timeline: "6 / 2015 - 12 / 2017",
+      image: `${Unis}`,
+      description:
+        "Owned all Ecommerce business operations including, but not limited to: order fulfillment, customer service, merchandising, SEO, and photo production",
+      skills: "Shopify, Lightspeed, Mailchimp, Excel, Photoshop",
+    },
+  ];
