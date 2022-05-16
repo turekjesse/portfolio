b0ec6c6aced1b716ef2185102f1bdb4568d6dcd8
turekjesse <turekjesse@gmail.com>
2022-05-16 19:26:41 -0400

    formatting + darkmode handler added

--- portfolio-v1/src/App.js
+++ portfolio-v1/src/App.js
@@ -1,67 +1,96 @@
-import { useState, useEffect } from 'react'
-import { Route } from 'react-router-dom'
-import './App.css';
+import { useState, useEffect } from "react";
+import { Route } from "react-router-dom";
+import "./App.css";
 
-import MobileNav from './components/Nav/MobileNav';
-import DesktopNav from './components/Nav/DesktopNav';
-import About from './components/About/About';
-import Experience from './components/Experience/Experience';
-import Hobbies from './components/Hobbies/Hobbies';
+import MobileNav from "./components/Nav/MobileNav";
+import DesktopNav from "./components/Nav/DesktopNav";
+import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
+import Hobbies from "./components/Hobbies/Hobbies";
 // import Home from './components/Home/Home';
-import Home2 from './components/Home/Home2';
-import Projects from './components/Projects/Projects';
-
+import Home2 from "./components/Home/Home2";
+import Projects from "./components/Projects/Projects";
 
 function App() {
-  const breakpoint = 990
+  // DARK-MODE HANDLER:
+  const dark = true;
+  const [mode, setMode] = useState(dark);
 
+  const handleDarkMode = () => {
+    setMode(!mode);
+    console.log(mode);
+  };
+  console.log(mode);
+
+  // BREAKPOINT HANDLER:
+  // WE USE THIS TO RENDER DIFFERENT COMPONENTS AT CERTAIN BREAKPOINTS
+  const breakpoint = 990;
   const useViewPortWidth = () => {
-    const [width, setWidth] = useState(window.innerWidth)
-  
+    const [width, setWidth] = useState(window.innerWidth);
+
     const handleWindowResize = () => {
-      setWidth(window.innerWidth)
-      window.addEventListener('resize', handleWindowResize)
-    }
-  
+      setWidth(window.innerWidth);
+      window.addEventListener("resize", handleWindowResize);
+    };
+
     const killWindowResize = () => {
-      return () => window.removeEventListener('resize', handleWindowResize)
-    } 
-  
+      return () => window.removeEventListener("resize", handleWindowResize);
+    };
+
     useEffect(() => {
-      handleWindowResize()
-      killWindowResize()
+      handleWindowResize();
+      killWindowResize();
       //eslint-disable-next-line
-    },[])
+    }, []);
 
     return { width };
-  }
+  };
 
   const { width } = useViewPortWidth();
 
   return (
     <>
-    {width < breakpoint ? <MobileNav /> : <DesktopNav />}
-    <Route path="/" exact render={() => <Home2 breakpoint={breakpoint} useViewPortWidth={useViewPortWidth}/>}/>
-    <Route
-      path="/about"
-      render={ () => <About breakpoint={breakpoint}
-      useViewPortWidth={useViewPortWidth} />}
-    />
-    <Route
-      path="/projects"
-      render={ () => <Projects breakpoint={breakpoint}
-      useViewPortWidth={useViewPortWidth} />}
-    />
-    <Route
-      path="/experience"
-      render={ () => <Experience breakpoint={breakpoint}
-      useViewPortWidth={useViewPortWidth} />}
-    />
-    <Route
-      path="/hobbies"
-      render={ () => <Hobbies breakpoint={breakpoint}
-      useViewPortWidth={useViewPortWidth} />}
-    />
+      {width < breakpoint ? <MobileNav /> : <DesktopNav />}
+      <Route
+        path="/"
+        exact
+        render={() => (
+          <Home2 breakpoint={breakpoint} useViewPortWidth={useViewPortWidth} />
+        )}
+      />
+      <Route
+        path="/about"
+        render={() => (
+          <About breakpoint={breakpoint} useViewPortWidth={useViewPortWidth} />
+        )}
+      />
+      <Route
+        path="/projects"
+        render={() => (
+          <Projects
+            breakpoint={breakpoint}
+            useViewPortWidth={useViewPortWidth}
+          />
+        )}
+      />
+      <Route
+        path="/experience"
+        render={() => (
+          <Experience
+            breakpoint={breakpoint}
+            useViewPortWidth={useViewPortWidth}
+          />
+        )}
+      />
+      <Route
+        path="/hobbies"
+        render={() => (
+          <Hobbies
+            breakpoint={breakpoint}
+            useViewPortWidth={useViewPortWidth}
+          />
+        )}
+      />
     </>
   );
 }
