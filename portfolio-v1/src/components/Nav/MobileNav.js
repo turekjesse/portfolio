35c52b3711a5ab2147f4f04a83fd5ff9d57f190f
turekjesse <turekjesse@gmail.com>
2022-05-19 06:07:21 -0400

    drk mode button added

--- portfolio-v1/src/components/Nav/MobileNav.js
+++ portfolio-v1/src/components/Nav/MobileNav.js
@@ -1,21 +1,29 @@
 import { Dropdown, Menu, Button } from "semantic-ui-react";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
-const MobileNav = () => {
+const MobileNav = ({ handleInverted, isInverted }) => {
+  const [isDisabled, setDisabled] = useState(false);
+
   return (
-    <Menu inverted>
+    <Menu inverted={isInverted}>
       <Dropdown item icon="bars" simple>
         <Dropdown.Menu inverted>
           <Menu.Item name="home" as={Link} to="/" />
           <Menu.Item as={Link} to="/about" name="about" />
           <Menu.Item as={Link} to="/projects" name="projects" />
           <Menu.Item as={Link} to="/experience" name="experience" />
-          {/* <Menu.Item as={Link} to="/hobbies" name="hobbies" /> */}
         </Dropdown.Menu>
       </Dropdown>
-      <Button.Group compact inverted>
-        <Button compact inverted icon="lightbulb" />
-        <Button compact icon="lightbulb outline" />
+      <Button.Group size="small" inverted>
+        <Button
+          color={isInverted ? "black" : "white"}
+          icon={isInverted ? "lightbulb outline" : "lightbulb"}
+          onClick={() => {
+            setDisabled(!isDisabled);
+            handleInverted();
+          }}
+        />
       </Button.Group>
     </Menu>
   );
